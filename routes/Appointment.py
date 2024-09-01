ec954201d3d5645abaab3f9e31aee08fa0da9b41
JuanTwo779 <juanmangubat@gmail.com>
2024-09-02 00:56:39 +1000

    Appointment model

---
+++ routes/Appointment.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Appointment(db.Model):
+     app_id = db.Column(db.Integer, primary_key=True)
+     patient_id = db.Column(db.Integer, nullable=False)
+     time = db.Column(db.Time, nullable=False)
+     date = db.Column(db.Date, nullable=False)
+
+     def __init__(self, patient_id, time, date):
+          self.patient_id = patient_id
+          self.time = time
+          self.date = date
+          
+
