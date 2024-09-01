422941c67b75009c22e6c221196d965201369e9e
JuanTwo779 <juanmangubat@gmail.com>
2024-09-02 00:37:37 +1000

    Patient model + model directory

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # create model
+from routes.Patient import Patient
+
 class Business(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(200), nullable=False, unique=True)
      phone = db.Column(db.String(12), nullable=False, unique=True)
      date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
-     def __init__(self, name, phone):
+     def __init__(self, name, phone, date_added):
           self.name = name
           self.phone = phone
+          self.date_added = date_added
 
-     # Create A String
-     def __repr__(self):
-          return f'<>'
 
 # Create route decorator
 @app.route("/")
---
+++ routes/Patient.py
@@ -0,0 +1,16 @@
+from app import db, datetime, timezone
+
+class Patient(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     business_id = db.Column(db.Integer, nullable=False)
+     first_name = db.Column(db.String(200), nullable=False, unique=True)
+     last_name = db.Column(db.String(200), nullable=False, unique=True)
+     phone = db.Column(db.String(12), nullable=False, unique=True)
+     date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+     def __init__(self, business_id, first_name, last_name, phone, date_added):
+          self.business_id = business_id
+          self.first_name = first_name
+          self.last_name = last_name
+          self.phone = phone
+          self.date_added = date_added
