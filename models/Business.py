702a94234b779b2af7642432adac63b2d800f570
JuanTwo779 <juanmangubat@gmail.com>
2024-09-02 14:21:51 +1000

    Model relationship adjustments

--- models/Business.py
+++ models/Business.py
@@ -3,8 +3,12 @@ from app import db, datetime, timezone
 class Business(db.Model):
      business_id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(200), nullable=False, unique=True)
-     phone = db.Column(db.String(12), nullable=False, unique=True)
+     phone = db.Column(db.String(15), nullable=False, unique=True)
      date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+     # One-to-many relationship (a business can have many patients)
+     patients = db.relationship('Patient', backref='business', lazy=True)
+     appointments = db.relationship('Appointment', backref='business', lazy=True)
 
      def __init__(self, name, phone, date_added):
           self.name = name
--- models/Patient.py
+++ models/Patient.py
@@ -2,11 +2,15 @@ from app import db, datetime, timezone
 
 class Patient(db.Model):
      patient_id = db.Column(db.Integer, primary_key=True)
-     business_id = db.Column(db.Integer, nullable=False)
+     business_id = db.Column(db.Integer, db.ForeignKey('business.business_id'), nullable=False)
      first_name = db.Column(db.String(200), nullable=False, unique=True)
      last_name = db.Column(db.String(200), nullable=False, unique=True)
-     phone = db.Column(db.String(12), nullable=False, unique=True)
+     phone = db.Column(db.String(15), nullable=False, unique=True)
      date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+     # relationships
+     business = db.relationship('Business', backref='patients', lazy=True)
+     appointments = db.relationship('Appointment', backref='patient', lazy=True)
 
      def __init__(self, business_id, first_name, last_name, phone, date_added):
           self.business_id = business_id
